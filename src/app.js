23f8e92c7105135fdd9bbcdd75a44a9a9e92acd8
Andreas Nauleau <anauleau@gmail.com>
2014-11-15 12:33:35 -0800

    Commit with basic UI

--- src/app.js
+++ src/app.js
@@ -2,4 +2,21 @@
  * Created by andreas on 11/7/14.
  */
 
+angular.module('traverse', [])
+  .controller('mainCtrl', ['$scope', function ($scope) {
+    console.log(_);
+    function Destination () {
+      this.destination;
+    }
+    $scope.destinations = [
+      {destination: undefined}
+    ];
 
+    $scope.addNewDestination = function(){
+      $scope.destinations.push(new Destination());
+    };
+
+    $scope.removeDestination = function (index) {
+      $scope.destinations = _.without($scope.destinations, $scope.destinations[index]);
+    }
+  }]);
