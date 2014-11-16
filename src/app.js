b2cc3a196c2758e12f7e1c6b8568afdb7792f983
Andreas Nauleau <anauleau@gmail.com>
2014-11-15 20:35:57 -0800

    Adds sorting by distanceFromOrigin

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ angular.module('traverse', [])
   .controller('mainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
     function Location () {
       this.address;
+      this.distanceFromOrigin;
+    }
+    function compare(a,b) {
+      if (a.distanceFromOrigin < b.distanceFromOrigin)
+        return -1;
+      if (a.distanceFromOrigin > b.distanceFromOrigin)
+        return 1;
+      return 0;
     }
     $scope.pointOfOrigin = new Location();
     $scope.destinations = [];
@@ -38,13 +46,17 @@ angular.module('traverse', [])
       });
       mapService.getDistanceMatrix(matrixArgs, function(d){
         $scope.results = d;
-        window.results = d;
         angular.forEach($scope.results.originAddresses, function(newAddress, key){
           if (key === 0) {
             $scope.pointOfOrigin.address = newAddress;
           } else {
             $scope.destinations[key - 1].address = newAddress;
+            $scope.destinations[key - 1].distanceFromOrigin = d.rows[0].elements[key].distance.text.replace(/\D/g, '');
           }
+        });
+        //
+        $scope.destinations.sort(function(a, b){
+          return a.distanceFromOrigin-b.distanceFromOrigin;
         });
       });
       $timeout(function(){
