0c261379efbdba296eb1498f539efcd45d6cc85e
Andreas Nauleau <anauleau@gmail.com>
2014-11-15 18:14:54 -0800

    adds distance matrix capability

--- src/app.js
+++ src/app.js
@@ -3,20 +3,45 @@
  */
 
 angular.module('traverse', [])
-  .controller('mainCtrl', ['$scope', function ($scope) {
-    console.log(_);
-    function Destination () {
-      this.destination;
+  .controller('mainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    function Location () {
+      this.address;
     }
-    $scope.destinations = [
-      {destination: undefined}
-    ];
+    $scope.pointOfOrigin = new Location();
+    $scope.destinations = [];
+
+    // Add first destination
+    $scope.destinations.push(new Location());
 
     $scope.addNewDestination = function(){
-      $scope.destinations.push(new Destination());
+      var newDest = new Location();
+      $scope.destinations.push(newDest);
     };
 
     $scope.removeDestination = function (index) {
       $scope.destinations = _.without($scope.destinations, $scope.destinations[index]);
-    }
+    };
+
+    $scope.getResults = function () {
+      $scope.loading = true;
+      var locations = [$scope.pointOfOrigin].concat($scope.destinations),
+        mapService = new google.maps.DistanceMatrixService(),
+        matrixArgs = {
+          destinations: [],
+          origins: [],
+          travelMode: 'DRIVING'
+        };
+
+      angular.forEach(locations, function (local) {
+        matrixArgs.origins.push(local.address);
+        matrixArgs.destinations.push(local.address);
+      });
+      mapService.getDistanceMatrix(matrixArgs, function(d){
+        $scope.results = d;
+        window.results = d;
+      });
+      $timeout(function(){
+        $scope.loading = false;
+      }, 1000);
+    };
   }]);
